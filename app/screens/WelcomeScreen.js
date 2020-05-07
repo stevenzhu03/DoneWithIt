44f67ca2f1f4b54ff7557b24823cfda666cfbd2f
Steven Zhu <stevenzhu03@gmail.com>
2020-05-06 22:38:46 -0400

    initialized project

---
+++ app/screens/WelcomeScreen.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { ImageBackground, StyleSheet, View, Image } from "react-native";
+
+function WelcomeScreen(props) {
+  return (
+    <ImageBackground
+      style={styles.background}
+      source={require("../assets/background.jpg")}
+    >
+      <Image style={styles.logo} sourece={require("../assets/logo-red.png")} />
+      <View style={styles.loginButton}></View>
+      <View style={styles.registerButton}></View>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  loginButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#fc5c65",
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    position: "absolute",
+    top: 70,
+  },
+  registerButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#4ecdc4",
+  },
+});
+
+export default WelcomeScreen;
